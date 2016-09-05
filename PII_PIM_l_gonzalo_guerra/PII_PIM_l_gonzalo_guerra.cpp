439ec1c6512a7442b01587c9264e8dc72fba9c18
Gonza <Gonza@DESKTOP-1692C01>
2016-09-05 20:06:09 -0300

    Agregar archivos de proyecto.

---
+++ PII_PIM_l_gonzalo_guerra/PII_PIM_l_gonzalo_guerra.cpp
@@ -0,0 +1,11 @@
+// PII_PIM_l_gonzalo_guerra.cpp: archivo de proyecto principal.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hola a todos");
+    return 0;
+}
---
+++ PII_PIM_l_gonzalo_guerra/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp: archivo de código fuente que contiene sólo las inclusiones estándar
+// PII_PIM_l_gonzalo_guerra.pch será el encabezado precompilado
+// stdafx.obj contiene la información de tipos precompilada
+
+#include "stdafx.h"
+
+
